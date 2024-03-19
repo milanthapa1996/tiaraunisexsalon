f600adef0f4ab0e3e2878ebbde2b19582f5dbfc8
Milan Thapa <tmilan0604@gmail.com>
2024-03-19 15:25:10 +0545

    code update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import HeroSection from "@/components/HeroSection";
 import Services from "@/components/Services";
+import Teams from "@/components/Teams";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
      <HeroSection/>
      <About/>
      <Services/>
+     <Teams/>
     </main>
   );
 }
